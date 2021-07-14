d8407c1dbc16030c0b1c54894c7fbcb9c5b213af
Arek <arek.glab97@gmail.com>
2021-07-14 20:07:53 +0200

    Added viewset to profiles api

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@2p-79+fe-wlkcdiqp%3^tx(m*^ett-b*nzbk_lbdg42a#&l1c'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
